15c4b10ed6a7f99d7d8f4f4803f2fd2291efd09b
Maxim_Anik <maksim9431@gmail.com>
2026-03-02 16:23:25 +0300

    feat: add HomePage and QuestionCard

---
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -0,0 +1,22 @@
+import { Button } from "../Button";
+import cls from "./QuestionCard.module.css";
+
+export const QuestionCard = () => {
+  return (
+    <div className={cls.card}>
+      <div className={cls.cardLabels}>
+        <div>Level: 1</div>
+        <div>Not Completed</div>
+      </div>
+
+      <h5 className={cls.cardTitle}>Что такое JSX ?</h5>
+
+      <div className={cls.cardAnswers}>
+        <label>short answer: </label>
+        <p className={cls.cardAnswer}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, voluptates.</p>
+      </div>
+
+      <Button onClick={() => {}}>View</Button>
+    </div>
+  );
+};
