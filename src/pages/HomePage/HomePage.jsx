ab79efd0a806c15652f03620401dc7607ece35ce
Maxim_Anik <maksim9431@gmail.com>
2026-03-05 19:01:51 +0300

    feat: add search input

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -19,8 +20,14 @@ export const HomePage = () => {
     getQuestions("react");
   }, []);
 
+  const onSearchChangeHandler = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <>
+      <input type="text" value={searchValue} onChange={onSearchChangeHandler} />
+
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
