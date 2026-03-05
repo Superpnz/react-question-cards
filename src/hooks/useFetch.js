8ea37200b9fbae531f804db502a7436d1654e65c
Maxim_Anik <maksim9431@gmail.com>
2026-03-05 15:42:36 +0300

    feat(hooks): add useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useFetch = (callback) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchFn = async (arg) => {
+    try {
+      setIsLoading(true);
+      setError("");
+
+      const response = await callback(arg);
+
+      return response;
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return [fetchFn, isLoading, error];
+};
