0854e7b1f23a5a517281c99a1866d243396d43e5
Maxim_Anik <maksim9431@gmail.com>
2026-03-11 15:48:07 +0300

    feat(pages): create question page

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-plugin-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -11,6 +12,7 @@ export default defineConfig([
     files: ["**/*.{js,jsx}"],
     plugins: {
       prettier,
+      react,
     },
     extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
@@ -24,6 +26,7 @@ export default defineConfig([
     },
     rules: {
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react/jsx-no-target-blank": "warn",
       "prettier/prettier": [
         "error",
         {
