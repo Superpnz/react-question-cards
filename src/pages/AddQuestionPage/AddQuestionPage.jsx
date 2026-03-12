3b9a960e7795a1074c2ba5b84f541ed45f321ec0
Maxim_Anik <maksim9431@gmail.com>
2026-03-12 09:49:47 +0300

    feat(AddQuestionPage): add error toast for failed question creation

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -22,12 +22,18 @@ const createCardAction = async (_prevState, formData) => {
         editDate: undefined,
       }),
     });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     const question = response.json();
     toast.success("New question is successfully created!");
 
     return isClearForm ? {} : question;
   } catch (error) {
     toast.error(error.message);
+    return {};
   }
 };
 
