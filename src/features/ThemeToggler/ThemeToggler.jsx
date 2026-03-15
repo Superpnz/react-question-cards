8efe14651da7a3716681eedfac4a17cc8c555e48
Maxim_Anik <maksim9431@gmail.com>
2026-03-15 14:58:06 +0300

    feat(theme): detect system color scheme

---
+++ src/features/ThemeToggler/ThemeToggler.jsx
@@ -0,0 +1,30 @@
+import { THEME_STORAGE } from "../../constants";
+import { useTheme } from "../../hooks/useTheme";
+import cls from "./ThemeToggler.module.css";
+
+export const ThemeToggler = () => {
+  const { theme, setTheme } = useTheme();
+
+  const onChangeHandler = (e) => {
+    const isChecked = e.target.checked;
+    const updatedTheme = isChecked ? "light" : "dark";
+
+    setTheme(updatedTheme);
+    // isChecked ? document.body.classList.add("darkLayout") : document.body.classList.remove("darkLayout");
+    if (updatedTheme === "dark") {
+      document.body.classList.add("darkLayout");
+    } else {
+      document.body.classList.remove("darkLayout");
+    }
+
+    localStorage.setItem(THEME_STORAGE, updatedTheme);
+  };
+
+  return (
+    <label htmlFor="switch" className={cls.switch}>
+      <input id="switch" type="checkbox" onChange={onChangeHandler} checked={theme === "light"} />
+      <span className={cls.slider} />
+      <span className={cls.decoration} />
+    </label>
+  );
+};
