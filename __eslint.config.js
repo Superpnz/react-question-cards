948606f9fe81376827c7658e207f58fdd01ed154
Maxim_Anik <maksim9431@gmail.com>
2026-03-30 15:56:33 +0300

    chore: setup TypeScript and ESLint configuration

--- __eslint.config.js
+++ __eslint.config.js
@@ -2,37 +2,31 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettierPlugin from "eslint-plugin-prettier";
+import prettier from "eslint-plugin-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx}"],
     plugins: {
-      prettier: prettierPlugin,
+      prettier,
+      react,
     },
+    extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
-      parser: tseslint.parser,
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
     },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^[A-Z_]",
-        },
-      ],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react/jsx-no-target-blank": "warn",
       "no-console": "warn",
       "prettier/prettier": [
         "error",
